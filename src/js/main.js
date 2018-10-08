3c58763827a332776ee78cf517063fdbfbaed0e1
Scott Harrison <github@scottharrisondesign.co.uk>
2018-10-08 13:48:34 +0100

    #1 initial phaser setup

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+    this.add.circle(config.width / 2, config.height / 2, 80, 0x6666ff);
+}
